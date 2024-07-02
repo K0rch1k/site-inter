185b92b85bad0f9ad8db86a252298506ef9087cc
K0rch1k <118677974+K0rch1k@users.noreply.github.com>
2024-07-02 14:52:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ let addToCartButton = document.querySelector("button");
 addToCartButton.addEventListener("click", function() {
     alert("Товар добавлен в корзину");
 });
+const singUpEl = document.querySelector("#singUP")
+
+const handleSingUpFormSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(singUpEl);
+    const newUser = Object.fromEntries(formData);
+
+    console.log("newUser", newUser)
+};
+
+singUpEl.addEventListener("submit", handleSingUpFormSubmit);
